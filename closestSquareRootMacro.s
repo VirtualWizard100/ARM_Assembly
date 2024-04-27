47d2bbff1bfd8b8f98ce6a9288d19d595cde741e
VirtualWizard100 <139145827+VirtualWizard100@users.noreply.github.com>
2024-04-27 09:51:05 -0400

    Update closestSquareRootMacro.s
    Correctly labeled the branches.

--- closestSquareRootMacro.s
+++ closestSquareRootMacro.s
@@ -3,22 +3,22 @@
 	ldr r0, =\num		@The number that were trying to find the square root of
 	mov r4, #1		@The perfect square square root incrementor
 
-loop:
+1:
 	mul r5, r4, r4		@r5 is r4 squared
 	cmp r5, r0		@Compare the squared number to r9
-	bgt Subtraction		@If r3 is greater than r9, then branch to subtraction
-	beq Congrats		@Else if r3 is equal to r9, branch to Congrats
+	bgt 2f          	@If r3 is greater than r9, then branch to subtraction
+	beq 3f   		@Else if r3 is equal to r9, branch to Congrats
 	add r4, #1		@Else add 1 to r4
 	eor r5, r5		@Clear r3
-	b loop			@Branch to loop
+	b 1b 			@Branch to loop
 
-Subtraction:
+2:
 	sub r4, #1		@Subtract 1 from r4
 	eor r5, r5		@Clear r3
 	mul r5, r4, r4		@r3 is r4 squared
 	sub r8, r0, r5		@Put into r8 r9 minus r3
 	lsl r9, r4, #1		@r5 is r4 multiplied by 2
-Congrats:
+3:
 	mov r0, r4
 	pop {r4-r12}
 .ENDM
